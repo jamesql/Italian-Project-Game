fa2c88b8e84b247284fbc9ba4455fe2f35c66727
James Ash <contactsmitteh@gmail.com>
2020-01-29 04:02:35 -0500

    Remove unnecessary code in StartGame.cs

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -1,31 +1,16 @@
-﻿ using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
- using UnityEngine.Events;
- using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour
-{
+public class StartGame: MonoBehaviour {
 
-    [SerializeField] UnityEvent anEvent;
+ [SerializeField] UnityEvent anEvent;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    // Methods
-    private void OnMouseDown()
-     {
-         print("Starting Game...");
-         SceneManager.LoadScene (sceneName:"SampleScene");
-     }
+ // On mouse down trigger this method
+ private void OnMouseDown() {
+  SceneManager.LoadScene(sceneName: "SampleScene");
+ }
 
 }
