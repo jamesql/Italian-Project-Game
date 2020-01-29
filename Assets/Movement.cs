252d1af068ede357c5a0a4c48d9c50cb676b6393
James Ash <contactsmitteh@gmail.com>
2020-01-29 04:01:20 -0500

    Beautify movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,56 +1,53 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
+public class Movement: MonoBehaviour {
 
-    private float speed = 2.0f;
-    public GameObject character;
-    
-     public float mouseSensitivity = 100.0f;
-     public float clampAngle = 80.0f;
- 
-     private float rotY = 0.0f; // rotation around the up/y axis
-     private float rotX = 0.0f; // rotation around the right/x axis
+ private float speed = 2.0 f;
+ public GameObject character;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Vector3 rot = transform.localRotation.eulerAngles;
-        rotY = rot.y;
-        rotX = rot.x;
-    }
+ public float mouseSensitivity = 100.0 f;
+ public float clampAngle = 80.0 f;
 
-    // Update is called once per frame
-    void Update () {
-		// Walking Movement
-		if (Input.GetKey(KeyCode.RightArrow)){
-			character.transform.position += Vector3.right * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.LeftArrow)){
-			character.transform.position += Vector3.left* speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.UpArrow)){
-			character.transform.position += Vector3.forward * speed * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.DownArrow)){
-			character.transform.position += Vector3.back* speed * Time.deltaTime;
-		}
+ private float rotY = 0.0 f; // rotation around the up/y axis
+ private float rotX = 0.0 f; // rotation around the right/x axis
 
-		// Looking Movement
-		 float mouseX = Input.GetAxis("Mouse X");
-         float mouseY = -Input.GetAxis("Mouse Y");
- 
-         rotY += mouseX * mouseSensitivity * Time.deltaTime;
-         rotX += mouseY * mouseSensitivity * Time.deltaTime;
- 
-         rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
- 
-         Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-         transform.rotation = localRotation;
+ // Start is called before the first frame update
+ void Start() {
+  Vector3 rot = transform.localRotation.eulerAngles;
+  rotY = rot.y;
+  rotX = rot.x;
+ }
 
+ // Update is called once per frame
+ void Update() {
+  // Walking Movement
+  if (Input.GetKey(KeyCode.RightArrow)) {
+   character.transform.position += Vector3.right * speed * Time.deltaTime;
+  }
+  if (Input.GetKey(KeyCode.LeftArrow)) {
+   character.transform.position += Vector3.left * speed * Time.deltaTime;
+  }
+  if (Input.GetKey(KeyCode.UpArrow)) {
+   character.transform.position += Vector3.forward * speed * Time.deltaTime;
+  }
+  if (Input.GetKey(KeyCode.DownArrow)) {
+   character.transform.position += Vector3.back * speed * Time.deltaTime;
+  }
 
-	}
+  // Looking Movement
+  float mouseX = Input.GetAxis("Mouse X");
+  float mouseY = -Input.GetAxis("Mouse Y");
+
+  rotY += mouseX * mouseSensitivity * Time.deltaTime;
+  rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+  rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+  Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0 f);
+  transform.rotation = localRotation;
+
+ }
 
 }
